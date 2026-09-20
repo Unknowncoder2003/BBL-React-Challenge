7815aba1900c04ee6ff8da799332bb34ad736574
Anon Tongngam <anon.tongngam@gmail.com>
2026-09-20 01:58:08 -0500

    got a list

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,25 +1,68 @@
-import { useState } from "react";
-import { StyleSheet, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Float } from "react-native/Libraries/Types/CodegenTypesNamespace";
+
+type ratings = {
+  rate: number;
+  count: number;  
+};
+
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: ratings;
+};
 
 export default function Index() {
+  //const [name, setName] = useState(0);
+
+  const [products, setProducts] = useState<Product[]>([]);
+  
+  const getFromApi = () => {
+  return fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(json => {
+      setProducts(json);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+  
+};
+
+useEffect(() => {
+    getFromApi();
+  }, []);
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.mytext}>Edit src/app/index.tsx to edit this screen.</Text>
+      <FlatList
+        data={products}
+        renderItem={({item}) => (
+          <Text style={styles.mytext}>{item.title}</Text>
+        )}
+        keyExtractor={item => item.id.toString()}
+      />
     </SafeAreaView>
   );
 }
-const [name, setName] = useState(0);
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
     backgroundColor: "black",
   },
   mytext: {
     color: "red",
+    fontSize: 18,
+    padding: 10,
   }
 
 });
